849cc8a3f782669b26ecde0a649bd65b063542ca
Marcel Holtmann <marcel@holtmann.org>
2013-12-09 16:05:35 +0100

    STPK-262: Add HCI user channel based bluedroid vendor adaptation
    This will create, bind and return an HCI sockets for bluedroid to use as
    a file descriptor (Instead of an fd on /dev/ttyS0).
    
    Issue: STPK-262
    Change-Id: I725b848fa1d1f6907a5f5e8ee31fb6d020bcc091
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(BOARD_HAVE_BLUETOOTH_LINUX), true)
+
+include $(CLEAR_VARS)
+
+BDROID_DIR := $(TOP_DIR)external/bluetooth/bluedroid
+
+LOCAL_SRC_FILES := \
+        bt_vendor_linux.c
+
+LOCAL_C_INCLUDES += \
+        $(BDROID_DIR)/hci/include
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils
+
+LOCAL_MODULE := libbt-vendor
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+AL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif # BOARD_HAVE_BLUETOOTH_LINUX
